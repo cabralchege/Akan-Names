c7b34f48088eb5f8fdc8d86b751fbbdaf50115cf
Cabral Chege <cabralowiro@gmail.com>
2020-02-17 01:04:58 +0300

    added the if else function in JS

--- Js/script.js
+++ Js/script.js
@@ -1,7 +1,30 @@
 var maleAkanNames = ["Kwasi", "Kwanndo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]   
 var femaleAkanNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
-var daysOfweek = ["Sunday", "Monday","Tuesady", "Wednesday", "Thursday", "Friday", "Saturday"]     
+var daysOfTheWeek = ["Sunday", "Monday","Tuesady", "Wednesday", "Thursday", "Friday", "Saturday"]     
 
  
+function outputName (){
+  var month=parseInt(document.getElementById("Month").value)
+  var day=parseInt(document.getElementById("Day").value)
+  var year=parseInt(document.getElementById("Year").value)
+  var gender =getGender();
 
+  var date =new Date(year +"/" + month + "/"+day);
+
+  var dayBorn = date.getDay();
+
+  var akanName;
+
+  if(gender ==="male"){
+      akanName = female[dayBorn]
+  }
+  else{
+      akanName = female[dayBorn]
+  }
+
+  alert("You were born on "+daysOfTheWeek[dayBorn])
+
+  
+
+}
  
