3c68f3ceaea141eebf943461e165882525b8d5cf
Cabral Chege <cabralowiro@gmail.com>
2020-02-17 10:31:53 +0300

    added the code if an invalid value is entered

--- Js/script.js
+++ Js/script.js
@@ -12,13 +12,13 @@ function getName(){
     var femaleNames = ["Akosua", "Adwoa", "Abenaa", "AKua", "Yaa", "Afua", "Ama"];
 
     if (year == "" || year.length < 4 || year.length > 4){
-        alert("Hey you need to enter a valid year!");
+        alert("Please  enter a valid year!");
     }
     else if (month < 0 || month > 12 ){
-        alert("Hey you need to enter a valid month!");
+        alert("Please enter a valid month!");
     }
     else if( dayOfMonth < 0 || dayOfMonth >31) {
-        alert("Hey you need to re-enter a valid day!");
+        alert("Please re-enter a valid day!");
     }
     else if (male.checked == false && female.checked == false){
         alert("You need to specify your gender!");
